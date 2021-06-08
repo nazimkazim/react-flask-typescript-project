9f620c6caafe46800d97dac553871b2b516c2279
nazimkazim <nazik_111@mail.ru>
2021-06-08 20:27:14 +0600

    add some changes

--- src/Components/Card/card.tsx
+++ src/Components/Card/card.tsx
@@ -13,8 +13,6 @@ interface TodosProps {
 export const Card: React.FC<TodosProps> = (props: TodosProps) => {
   const { todos, deleteItem } = props
 
-
-
   return <div>
     {todos && todos.map(item => <li key={item.id}><Link to={`${item.id}`}>{item.content}</Link><button onClick={() => deleteItem(item.id)}>Delete</button></li>)}
   </div>
